0a7d4de601786d9ec920ed77c42ce42f29e930b3
Sergey Gorovov <gor-serg@bk.ru>
2020-05-17 14:59:18 +0300

    added creating test case through passing an iterable

--- src/code_speed_test.py
+++ src/code_speed_test.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import time
-from typing import Callable, Tuple, List
+from typing import Callable, Tuple, List, Union, Iterable
 
 import matplotlib.pyplot as plt
 
@@ -12,62 +12,91 @@ class _TestCase:
                  start: int,
                  stop: int = None,
                  step: int = 1,
-                 total_time: int = None,
-                 max_time: int = None,
+                 time_limit: int = None,
                  ) -> None:
         self.func = func
         self.start = start
         self.stop = stop
         self.step = step
-        self.total_time = total_time
-        self.max_time = max_time
+        self.time_limit = time_limit
 
 
-def _compute_case(case: _TestCase) -> Tuple[List[int], List[float]]:
+class _DataTestCase:
+    def __init__(self,
+                 func: Callable,
+                 arguments: Iterable,
+                 time_limit: int = None,
+                 ) -> None:
+        self.func = func
+        self.arguments = arguments
+        self.time_limit = time_limit
+
+
+def _compute_range_case(case: _TestCase) -> Tuple[List[int], List[float]]:
     arg = case.start
-    x: List[int] = []
-    y: List[float] = []
-    time_spent = 0
-    while case.total_time > 0 if case.total_time else True:
+    arguments: List[int] = []
+    times: List[float] = []
+    while case.time_limit > 0 if case.time_limit else True:
         if case.stop and arg >= case.stop:
-            break
-        if case.max_time and time_spent > case.max_time:
             break
         start_time = time.time()
         case.func(arg)
         time_spent = time.time() - start_time
-        y.append(time_spent)
-        if case.total_time:
-            case.total_time -= time_spent
-        x.append(arg)
+        times.append(time_spent)
+        if case.time_limit:
+            case.time_limit -= time_spent
+        arguments.append(arg)
         arg += case.step
-    return x, y
+    return arguments, times
+
+
+def _compute_args_case(case: _DataTestCase) -> Tuple[List[int], List[float]]:
+    arguments: List[int] = []
+    times: List[float] = []
+    time_spent = 0
+    for i in case.arguments:
+        if time_spent > case.time_limit:
+            break
+        start_time = time.time()
+        case.func(i)
+        time_spent = time.time() - start_time
+        times.append(time_spent)
+        if case.time_limit:
+            case.time_limit -= time_spent
+        arguments.append(i)
+    return arguments, times
 
 
 class Tester:
     def __init__(self) -> None:
-        self.test_cases: List[_TestCase] = []
+        self.test_cases: List[Union[_TestCase, _DataTestCase]] = []
         self.results: List[Tuple[List[int], List[float]]] = []
 
     def add_test_case(self,
                       func: Callable,
-                      start: int,
+                      start: int = 0,
                       stop: int = None,
                       step: int = 1,
                       *,
-                      total_time: int = 60,
-                      max_time: int = None,
+                      arguments: Iterable = None,
+                      time_limit: int = 60,
                       ):
-        self.test_cases.append(_TestCase(func, start, stop, step, total_time, max_time))
+        if arguments:
+            self.test_cases.append(_DataTestCase(func, arguments, time_limit))
+        else:
+            self.test_cases.append(_TestCase(func, start, stop, step, time_limit))
 
     def start_computations(self) -> None:
         for i in self.test_cases:
-            self.results.append(_compute_case(i))
+            if isinstance(i, _TestCase):
+                self.results.append(_compute_range_case(i))
+            elif isinstance(i, _DataTestCase):
+                self.results.append(_compute_args_case(i))
 
     def show_plot(self) -> None:
         for case, result in zip(self.test_cases, self.results):
             plt.plot(result[0], result[1], label=case.func.__name__)
-        plt.title("CodeTestSpeed")
+        plt.title("CodeSpeedTest")
         plt.xlabel("iterations")
         plt.ylabel("time, sec")
         plt.legend()
--- src/example.py
+++ src/example.py
@@ -17,8 +17,17 @@ def append_sort_test(n: int) -> None:
     _ = sorted(lst)
 
 
+# 1. Create tester object
 tester = code_speed_test.Tester()
-tester.add_test_case(insort_test, 0, step=10000)
-tester.add_test_case(append_sort_test, 0, step=10000)
+
+# 2. Adding tests
+# a) creating data list by assigning start (optional stop) and step for range of arguments
+tester.add_test_case(insort_test, start=0, step=10000, time_limit=5)
+
+# b) directly passing iterable object of test cases
+test_args = [1, 10, 10000, 15000, 100000, 200000, 300000]
+tester.add_test_case(append_sort_test, arguments=test_args, time_limit=5)
+
+# 3. Running tests, and plotting the graph
 tester.start_computations()
 tester.show_plot()
